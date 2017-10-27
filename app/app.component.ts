545b57d38bff978d0311cb3a101e52f162c8fa99
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-10-27 09:20:56 -0700

    set up page with class animal

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-  <h1>Local Zoo</h1>
+  <div class="container">
+    <h1>Local Zoo</h1>
+  </div>
   `
+
 })
+
+export class AppComponent {
+
+}
+
+export class Animal {
+  constructor(public species: string, public name: string, public age: number, public diet: string, public location: string, public caretakers: number, public sex: string, public likes: string, public dislikes: string) { }
+}
