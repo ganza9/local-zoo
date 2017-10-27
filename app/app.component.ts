c6ca173dec3fb17f6183224e4f6c6e8664eff138
Gennaro Anzalone <g.anzalone12@gmail.com>
2017-10-27 10:26:52 -0700

    fill out animal class

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,12 +5,28 @@ import { Component } from '@angular/core';
   template: `
   <div class="container">
     <h1>Local Zoo</h1>
+
+    <ul *ngFor="let currentAnimal of animals">
+      <li>{{currentAnimal.species}}</li>
+      <li>{{currentAnimal.name}}</li>
+      <li>{{currentAnimal.age}}</li>
+      <li>{{currentAnimal.diet}}</li>
+      <li>{{currentAnimal.location}}</li>
+      <li>{{currentAnimal.caretakers}}</li>
+      <li>{{currentAnimal.sex}}</li>
+      <li>{{currentAnimal.likes}}</li>
+      <li>{{currentAnimal.dislikes}}</li>
+    </ul>
   </div>
   `
-
 })
 
 export class AppComponent {
+  animals: Animal[] = [
+    new Animal ('Arctic Fox', 'Moon', 2, 'Carnivore', 'Northern Trail', 5, 'Female', 'Cool shade', 'Loud noises'),
+    new Animal ('Ocelot', 'Prince', 4, 'Carnivore', 'Tropical Rain Forest Building', 6, 'Male', 'Laying in the sun', 'Toys that are non rope based'),
+    new Animal ('Northwest Black Tailed Deer', 'Tinkerbell', 8, 'Herbivore', 'Northern Trail', 2, 'Female', 'Delicate roots and leaves', 'Loud noises')
+  ]
 
 }
 
